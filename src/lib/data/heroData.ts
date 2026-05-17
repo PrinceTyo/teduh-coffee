c94ce1e2bdb775018999310d16a9843747a7152e
PrinceTyo <kresnaale2@gmail.com>
2026-05-17 21:49:48 +0700

    add description text in hero-section

--- src/lib/data/heroData.ts
+++ src/lib/data/heroData.ts
@@ -5,6 +5,6 @@ export const heroData = {
   badge: "Welcome to Teduh Coffee",
   title: "Temukan Teduh Pada\nSetiap Tegukan",
   description:
-    "Teduh Coffee hadir sebagai ruang nyaman untuk menikmati kopi...",
+    "Teduh Coffee hadir sebagai ruang nyaman untuk menikmati kopi, berbagi cerita, dan menikmati waktu dengan suasana hangat, tenang, dan modern di tengah hiruk pikuk kota.",
   buttonText: "Explore Menu",
 };
